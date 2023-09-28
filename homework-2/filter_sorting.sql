fd64571c165943d7fe3a2e2ac58aa743764e3bd5
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-28 14:09:53 +0300

    filter_sorting - 5

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -20,3 +20,6 @@ FROM products
 WHERE discontinued <> 1
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+SELECT MIN (unit_price), MAX (unit_price)
+FROM products
+WHERE discontinued <> 1 AND units_in_stock >= 20
