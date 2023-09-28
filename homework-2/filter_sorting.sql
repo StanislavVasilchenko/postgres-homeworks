5f8fdaaafb6eca013299613ac0b98d56a93544d2
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-28 14:04:13 +0300

    filter_sorting - 3

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -10,7 +10,9 @@ FROM orders
 ORDER BY ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-
+SELECT AVG (shipped_date - order_date)
+FROM orders
+WHERE ship_country = 'Germany'
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 
