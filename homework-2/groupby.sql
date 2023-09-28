2f2871b7b3e768510d10b300ce1e809dac93b443
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-28 15:11:57 +0300

    groupby - 6

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -32,6 +32,10 @@ HAVING  SUM (freight) > 2750
 ORDER BY SUM (freight) DESC
 
 -- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
-
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
+INTERSECT
+SELECT country FROM employees
 
 -- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
