7c4cf6ed3c3c71febaa3fef3914f628817a3e529
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-28 13:54:17 +0300

    filter_sorting - 2

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -5,7 +5,9 @@ FROM orders
 WHERE ship_country IN ('France', 'Germany', 'Spain')
 
 -- 2. уникальные страны и города, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
-
+SELECT DISTINCT ship_country, ship_city
+FROM orders
+ORDER BY ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 
