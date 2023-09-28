6508d499af319af66f19708e850cb99f9c6251d5
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-28 10:55:22 +0300

    simple_queries - 2

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -4,7 +4,8 @@ SELECT contact_name, city
 FROM customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-
+SELECT order_id, (shipped_date - order_date)
+FROM orders
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
 
