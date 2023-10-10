4f0a131ab166c7009ef9204c8eaec6a4370f6a9c
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-10-10 21:29:51 +0300

    Реализация функции execute_sql_script

--- homework-5/main.py
+++ homework-5/main.py
@@ -16,28 +16,28 @@ def main():
     create_database(params, db_name)
     print(f"БД {db_name} успешно создана")
 
-    # params.update({'dbname': db_name})
-    # try:
-    #     with psycopg2.connect(**params) as conn:
-    #         with conn.cursor() as cur:
-    #             execute_sql_script(cur, script_file)
-    #             print(f"БД {db_name} успешно заполнена")
-    #
-    #             create_suppliers_table(cur)
-    #             print("Таблица suppliers успешно создана")
-    #
-    #             suppliers = get_suppliers_data(json_file)
-    #             insert_suppliers_data(cur, suppliers)
-    #             print("Данные в suppliers успешно добавлены")
-    #
-    #             add_foreign_keys(cur, json_file)
-    #             print(f"FOREIGN KEY успешно добавлены")
-    #
-    # except(Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    params.update({'dbname': db_name})
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                execute_sql_script(cur, script_file)
+                print(f"БД {db_name} успешно заполнена")
+
+                # create_suppliers_table(cur)
+                # print("Таблица suppliers успешно создана")
+                #
+                # suppliers = get_suppliers_data(json_file)
+                # insert_suppliers_data(cur, suppliers)
+                # print("Данные в suppliers успешно добавлены")
+                #
+                # add_foreign_keys(cur, json_file)
+                # print(f"FOREIGN KEY успешно добавлены")
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 def create_database(params, db_name) -> None:
@@ -53,7 +53,9 @@ def create_database(params, db_name) -> None:
 
 def execute_sql_script(cur, script_file) -> None:
     """Выполняет скрипт из файла для заполнения БД данными."""
-    pass
+    with open(script_file, "r", encoding="utf-8") as script:
+        result = script.read()
+    cur.execute(result)
 
 
 def create_suppliers_table(cur) -> None:
